c114a1425e3b32c2bc28ec0067cd06cae424beec
Nuriddin Shahobov <100368507+nuriddinshahobov2001@users.noreply.github.com>
2023-05-06 15:19:53 +0500

    git commi

--- resources/views/client/layouts/app.blade.php
+++ resources/views/client/layouts/app.blade.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
 
-    <link rel="stylesheet" href="{{asset('admin/assets/css/main/app.css')}}">
-    <link rel="stylesheet" href="{{asset('admin/assets/css/main/app-dark.css')}}">
-    <link rel="shortcut icon" href="{{asset('admin/assets/images/logo/favicon.svg')}}" type="image/x-icon">
-    <link rel="shortcut icon" href="{{asset('admin/assets/images/logo/favicon.png')}}" type="image/png">
-    <link rel="stylesheet" href="{{asset('admin/assets/css/shared/iconly.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/main/app.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/main/app-dark.css')}}">
+    <link rel="shortcut icon" href="{{asset('assets/images/logo/favicon.svg')}}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{asset('assets/images/logo/favicon.png')}}" type="image/png">
+    <link rel="stylesheet" href="{{asset('assets/css/shared/iconly.css')}}">
     @yield('content')
 </head>
 
@@ -22,12 +22,12 @@
 
 
 
-<script src="{{asset('admin/assets/js/bootstrap.js')}}"></script>
-<script src="{{asset('admin/assets/js/app.js')}}"></script>
+<script src="{{asset('assets/js/bootstrap.js')}}"></script>
+<script src="{{asset('assets/js/app.js')}}"></script>
 
 @yield('script')
-<script src="{{asset('admin/assets/extensions/apexcharts/apexcharts.min.js')}}"></script>
-<script src="{{asset('admin/assets/js/pages/dashboard.js')}}"></script>
+<script src="{{asset('assets/extensions/apexcharts/apexcharts.min.js')}}"></script>
+<script src="{{asset('assets/js/pages/dashboard.js')}}"></script>
 
 </body>
 
