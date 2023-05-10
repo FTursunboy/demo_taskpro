ec7c9e0b1828d9b5554d8ae098e82611d83261cc
Nuriddin Shahobov <100368507+nuriddinshahobov2001@users.noreply.github.com>
2023-05-10 17:35:36 +0500

    editionm

--- resources/views/admin/monitoring/tasks.blade.php
+++ resources/views/admin/monitoring/tasks.blade.php
@@ -85,7 +85,7 @@
                         <div class="form-group">
                             <label for="type">Тип</label>
                             <input type="text" id="type" class="form-control"
-                                   value="{{ $task->type->name }} {{  (isset($task->typeType->name)) ? '- '.$task->typeType->name : '' }}"
+                                   value="{{ $task->type?->name }} {{  (isset($task->typeType?->name)) ? '- '.$task->typeType?->name : '' }}"
                                    disabled>
                         </div>
                         <div class="form-group">
@@ -635,7 +635,7 @@
                         <div class="form-group">
                             <label for="type">Тип</label>
                             <input type="text" id="type" class="form-control"
-                                   value="{{ $task->type->name }} {{  (isset($task->typeType->name)) ? '- '.$task->typeType->name : '' }}"
+                                   value="{{ $task->type?->name }} {{  (isset($task->typeType?->name)) ? '- '.$task->typeType?->name : '' }}"
                                    disabled>
                         </div>
                         <div class="form-group">
