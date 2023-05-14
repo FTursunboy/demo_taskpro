675daa5e7074e655d8800e70f87fe8e06f1610fe
Tursunboy Faiziev <107097209+FTursunboy@users.noreply.github.com>
2023-05-14 14:17:27 +0500

    132

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@ Route::group(['middleware' => 'redirectIfUnauthorized'], function () {
     require __DIR__ . '/admin/admin.php';
     require __DIR__ . '/user/user.php';
     require __DIR__ . '/client/client.php';
-    require __DIR__ . '/auth.php';
 });
+require __DIR__ . '/auth.php';
 
 Route::get('/logout', function (){
    \Illuminate\Support\Facades\Auth::logout();
