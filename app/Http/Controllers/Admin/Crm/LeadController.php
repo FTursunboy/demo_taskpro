9e16fa4312b26800e02f5f939dd8fe09e5cc2610
Tursunboy Faiziev <107097209+FTursunboy@users.noreply.github.com>
2023-05-23 16:51:47 +0500

    123

--- app/Http/Controllers/Admin/Crm/LeadController.php
+++ app/Http/Controllers/Admin/Crm/LeadController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Admin\Crm;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\CRM\Lead;
 use Illuminate\Http\Request;
 
 class LeadController extends Controller
 {
-    //
+    public function index() {
+
+    }
 }
--- app/Models/Admin/CRM/Lead.php
+++ app/Models/Admin/CRM/Lead.php
@@ -9,4 +9,17 @@ class Lead extends Model
 {
     use HasFactory;
     protected $fillable = ['contact_id', 'lead_status_id', 'lead_source_id', 'lead_state_id'];
+
+    public function leadSource() {
+        return $this->belongsTo(LeadSource::class, 'lead_source_id');
+    }
+    public function status(){
+        return $this->belongsTo(LeadStatus::class, 'lead_status_id');
+    }
+    public function contact(){
+        return $this->belongsTo(Contact::class, 'contact_id');
+    }
+    public function state() {
+        return $this->belongsTo(LeadState::class, 'lead_state_id');
+    }
 }
