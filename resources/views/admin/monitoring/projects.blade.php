f7cdb32580ca5cbd315060f77c79c9103fdf9c52
Tursunboy Faiziev <107097209+FTursunboy@users.noreply.github.com>
2023-06-05 09:00:00 +0500

    123

--- resources/views/admin/monitoring/projects.blade.php
+++ resources/views/admin/monitoring/projects.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group">
-    <label for="project_id">Проекти</label>
+    <label for="project_id">Проекты</label>
     <select name="project_id" id="project_id" class="form-select">
-        <option value="0" selected>Все проекти</option>
+        <option value="0" selected>Все проекты</option>
         @foreach($projects as $project)
             <option value="{{ $project->id }}">{{ $project->name }}</option>
         @endforeach
