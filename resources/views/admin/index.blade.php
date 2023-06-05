ebf833e4d6f9181fac3e0a7d21eef9231f60a253
Tursunboy Faiziev <107097209+FTursunboy@users.noreply.github.com>
2023-06-05 15:06:04 +0500

    123

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -58,7 +58,7 @@
                                                 </div>
                                             </div>
                                             <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                                <h6 class="text-muted font-semibold">Просроченный</h6>
+                                                <h6 class="text-muted font-semibold">Просроченные</h6>
                                                 <h6 class="font-extrabold mb-0">{{ $task['speed'] }}</h6>
                                             </div>
                                         </div>
