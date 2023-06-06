4bf3902f4351b413f4c1541dd6225446a8fde51d
Nuriddin Shahobov <100368507+nuriddinshahobov2001@users.noreply.github.com>
2023-06-06 13:35:26 +0500

    asdas

--- resources/views/client/offers/index.blade.php
+++ resources/views/client/offers/index.blade.php
@@ -1,112 +1,113 @@
 @extends('client.layouts.app')
 @section('content')
 
-            <div class="page-heading">
-                <div class="page-title">
-                    <div class="row">
-                        <div class="col-12 col-md-6 order-md-1 order-last">
-                            <h3>Задачи</h3>
-                        </div>
-                        <div class="col-12 col-md-6 order-md-2 order-first">
-                            <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="{{route('offers.index')}}">Задачи</a></li>
-
-                                </ol>
-                            </nav>
-                        </div>
-                    </div>
+    <div class="page-heading">
+        <div class="page-title">
+            <div class="row">
+                <div class="col-12 col-md-6 order-md-1 order-last">
+                    <h3>Задачи</h3>
                 </div>
-                @include('inc.messages')
-                <section class="section">
-                    <div class="card">
-                        <div class="card-header">
-                            <a href="{{route('offers.create')}}"  class="btn btn-primary">Добавить</a>
-                        </div>
-                        <div class="card-body">
-                            <table class="table table-striped" id="table1">
-                                <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Название</th>
-                                    <th>Описание</th>
-                                    <th>Статус</th>
-                                    <th>Действие</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @forelse($tasks as $task)
-                                    <tr>
-                                    <td>{{$loop->iteration}}</td>
-                                    <td>{{$task->name}}</td>
-                                    <td>{{\Illuminate\Support\Str::limit($task->description, 20)}}</td>
-                                        @if($task->status->id == 1)
-                                            <td><span class="badge bg-success p-2">{{$task->status->name}}</span>
-                                            </td>
-                                        @elseif($task->status->id == 2)
-                                            <td><span class="badge bg-primary p-2">{{$task->status->name}}</span>
-                                            </td>
-                                        @elseif($task->status->id == 3)
-                                            <td><span class="badge bg-warning p-2">{{$task->status->name}}</span>
-                                            </td>
-                                        @elseif($task->status->id == 4)
-                                            <td><span class="badge bg-warning p-2">{{$task->status->name}}</span>
-                                            </td>
-                                        @elseif($task->status->id == 5)
-                                            <td><span class="badge bg-warning p-2">{{$task->status->name}}</span>
-                                            </td>
-                                        @elseif($task->status->id == 6)
-                                            <td><span class="badge bg-primary p-2">На проверке (У админа)</span>
-                                            </td>
-                                        @elseif($task->status->id == 7)
-                                            <td><span class="badge bg-warning p-2">{{$task->status->name}}</span>
-                                            </td>
-                                        @elseif($task->status->id == 8)
-                                            <td><span class="badge bg-warning p-2">{{$task->status->name}}</span>
-                                            </td>
-                                        @elseif($task->status->id == 9)
-                                            <td><span class="badge bg-warning p-2">{{$task->status->name}}</span>
-                                            </td>
-                                        @elseif($task->status->id == 10)
-                                            <td><span class="badge bg-danger p-2">{{$task->status->name}}</span>
-                                            </td>
-                                        @elseif($task->status->id == 11)
-                                            <td><span class="badge bg-warning p-2">{{$task->status->name}}</span>
-                                            </td>
-                                        @elseif($task->status->id == 12)
-                                            <td><span class="badge bg-danger p-2">{{$task->status->name}}</span>
-                                            </td>
-                                        @elseif($task->status->id == 13)
-                                            <td><span class="badge bg-danger p-2">{{$task->status->name}}</span>
-                                            </td>
-                                        @elseif($task->status->id == 14)
-                                            <td><span class="badge bg-warning p-2">{{$task->status->name}}</span>
-                                            </td>
-                                        @endif
-                                    <td>
+                <div class="col-12 col-md-6 order-md-2 order-first">
+                    <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{route('offers.index')}}">Задачи</a></li>
 
-                                        <a class="badge bg-success p-2" href="{{route('offers.show', $task->id)}}"><i class="bi bi-eye"></i></a>
-                                        @if($task->status->id == 8)
-                                            <a class=" badge bg-primary p-2" href="{{route('offers.edit', $task->id)}}"><i class="bi bi-pencil"></i></a>
-                                        @endif
-
-
-                                            <a class=" badge bg-warning p-2" href="{{route('offers.chat', $task->id)}}"><i class="bi bi-chat"></i></a>
-
-
-                                    </td>
-                                    </tr>
-                                @empty
-                                    <td  colspan="5"><h1 class="text-center">Пока нет задач</h1></td>
-                                @endforelse
-
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-
-                </section>
+                        </ol>
+                    </nav>
+                </div>
             </div>
+        </div>
+        @include('inc.messages')
+        <section class="section">
+            <div class="card">
+                <div class="card-header">
+                    <a href="{{route('offers.create')}}" class="btn btn-primary">Добавить</a>
+                </div>
+                <div class="card-body">
+                    <table class="table table-striped" id="table1">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Название</th>
+                            <th>Статус</th>
+                            <th>Действие</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @forelse($tasks as $task)
+                            <tr>
+                                <td>{{$loop->iteration}}</td>
+                                <td>{{\Str::limit($task->name,50)}}</td>
+                                @if($task->status->id == 1)
+                                    <td><span class="badge bg-success p-2">{{$task->status->name}}</span>
+                                    </td>
+                                @elseif($task->status->id == 2)
+                                    <td><span class="badge bg-primary p-2">{{$task->status->name}}</span>
+                                    </td>
+                                @elseif($task->status->id == 3)
+                                    <td><span class="badge bg-warning p-2">{{$task->status->name}}</span>
+                                    </td>
+                                @elseif($task->status->id == 4)
+                                    <td><span class="badge bg-warning p-2">{{$task->status->name}}</span>
+                                    </td>
+                                @elseif($task->status->id == 5)
+                                    <td><span class="badge bg-warning p-2">{{$task->status->name}}</span>
+                                    </td>
+                                @elseif($task->status->id == 6)
+                                    <td><span class="badge bg-primary p-2">На проверке (У админа)</span>
+                                    </td>
+                                @elseif($task->status->id == 7)
+                                    <td><span class="badge bg-warning p-2">{{$task->status->name}}</span>
+                                    </td>
+                                @elseif($task->status->id == 8)
+                                    <td><span class="badge bg-warning p-2">{{$task->status->name}}</span>
+                                    </td>
+                                @elseif($task->status->id == 9)
+                                    <td><span class="badge bg-warning p-2">{{$task->status->name}}</span>
+                                    </td>
+                                @elseif($task->status->id == 10)
+                                    <td><span class="badge bg-danger p-2">{{$task->status->name}}</span>
+                                    </td>
+                                @elseif($task->status->id == 11)
+                                    <td><span class="badge bg-warning p-2">{{$task->status->name}}</span>
+                                    </td>
+                                @elseif($task->status->id == 12)
+                                    <td><span class="badge bg-danger p-2">{{$task->status->name}}</span>
+                                    </td>
+                                @elseif($task->status->id == 13)
+                                    <td><span class="badge bg-danger p-2">{{$task->status->name}}</span>
+                                    </td>
+                                @elseif($task->status->id == 14)
+                                    <td><span class="badge bg-warning p-2">{{$task->status->name}}</span>
+                                    </td>
+                                @endif
+                                <td>
+
+                                    <a class="badge bg-success p-2" href="{{route('offers.show', $task->id)}}"><i
+                                                class="bi bi-eye"></i></a>
+                                    @if($task->status->id == 8)
+                                        <a class=" badge bg-primary p-2" href="{{route('offers.edit', $task->id)}}"><i
+                                                    class="bi bi-pencil"></i></a>
+                                    @endif
+
+
+                                    <a class=" badge bg-warning p-2" href="{{route('offers.chat', $task->id)}}"><i
+                                                class="bi bi-chat"></i></a>
+
+
+                                </td>
+                            </tr>
+                        @empty
+                            <td colspan="5"><h1 class="text-center">Пока нет задач</h1></td>
+                        @endforelse
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </section>
+    </div>
 
 
 @endsection
