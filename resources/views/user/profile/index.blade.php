dd1fc5d5b3726772a001d2a8fda470957c99c5e0
Nuriddin Shahobov <100368507+nuriddinshahobov2001@users.noreply.github.com>
2023-06-06 08:44:47 +0500

    asdf

--- resources/views/user/profile/index.blade.php
+++ resources/views/user/profile/index.blade.php
@@ -119,7 +119,7 @@
                                 </div>
                                 <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                     <h6 class="text-muted font-semibold">Отклоненные</h6>
-                                    <h6 class="font-extrabold mb-0">{{ $task['unSuccess'] }}</h6>
+                                    <h6 class="font-extrabold mb-0">{{ $task['inProgress'] }}</h6>
                                 </div>
                             </div>
                         </div>
