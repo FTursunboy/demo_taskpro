a6d496c349898d977b7d4b35b05aabd582873fa3
Nuriddin Shahobov <nuriddinshahobov@gmail.com>
2023-06-07 06:05:11 +0500

    add kpi

--- resources/views/user/task/index.blade.php
+++ resources/views/user/task/index.blade.php
@@ -127,7 +127,7 @@
                                                             <div class="form-group">
                                                                 <label for="type">Тип</label>
                                                                 <input type="text" id="type" class="form-control"
-                                                                       value="{{ $task->type?->name }} {{  (isset($task->typeType->name)) ? '- '.$task->typeType->name : '' }}"
+                                                                       value="{{ $task->type?->name }} {{  (isset($task->typeType->name)) ? '- '.$task->typeType->name .'- '.$task->percent : '' }}"
                                                                        disabled>
                                                             </div>
                                                             <div class="form-group">
