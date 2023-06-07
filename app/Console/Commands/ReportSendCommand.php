ad64871c933fd79cdc4739952e90b408ddcb258b
Tursunboy Faiziev <107097209+FTursunboy@users.noreply.github.com>
2023-06-07 08:50:54 +0500

    123

--- app/Console/Commands/ReportSendCommand.php
+++ app/Console/Commands/ReportSendCommand.php
@@ -103,9 +103,9 @@ class ReportSendCommand extends Command
         $report->save();
 
         $files = storage_path('app/public/' . $report->file);
-        $email = EmailModel::first()->email;
-        Mail::send([], [], function ($message) use ($files, $email) {
-            $message->to($email)
+
+        Mail::send([], [], function ($message) use ($files) {
+            $message->to('amr_1990@mail.ru')
                 ->subject('Отчет')
                 ->attach($files, ['as' => 'Отчет_этого_дня.xlsx', 'mime' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']);
         });
