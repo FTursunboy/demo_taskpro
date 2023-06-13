7235173d3f4d45a52ff2a27ca6efbbfda4988bf0
Nuriddin Shahobov <100368507+nuriddinshahobov2001@users.noreply.github.com>
2023-06-13 17:03:36 +0500

    fasdf

--- resources/views/client/incs/header.blade.php
+++ resources/views/client/incs/header.blade.php
@@ -7,7 +7,7 @@
     }
 </style>
 
-<header class='mb-3'>
+<header class='mb-3 bg-white'>
     <nav class="navbar navbar-expand navbar-light navbar-top">
         <div class="container-fluid">
             <a href="#" class="burger-btn d-block">
@@ -21,71 +21,6 @@
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-lg-0">
-
-
-
-{{--                    <li class="nav-item dropdown me-3">--}}
-{{--                        <a class="nav-link active dropdown-toggle text-gray-600" href="#"--}}
-{{--                           data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">--}}
-{{--                            <i class='bi bi-bell{{ (count($newMessage) > 0) ? '-fill' : '' }} fs-4 {{ (count($newMessage) > 0) ? 'highlight-icon' : '' }}'></i>--}}
-{{--                        </a>--}}
-{{--                        <ul class="dropdown-menu dropdown-menu-end notification-dropdown"--}}
-{{--                            aria-labelledby="dropdownMenuButton">--}}
-{{--                            <li class="dropdown-header">--}}
-
-{{--                                <h6></h6>--}}
-{{--                            </li>--}}
-{{--                            <li class="dropdown-item notification-item">--}}
-{{--                                <a class="d-flex align-items-center" href="#">--}}
-{{--                                    <div class="notification-icon bg-primary">--}}
-{{--                                        <i class="bi bi-cart-check"></i>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="notification-text ms-4">--}}
-{{--                                        <p class="notification-title font-bold">Successfully check out</p>--}}
-{{--                                        <p class="notification-subtitle font-thin text-sm">Order ID #256</p>--}}
-{{--                                    </div>--}}
-{{--                                </a>--}}
-{{--                            </li>--}}
-{{--                            <li class="dropdown-item notification-item">--}}
-{{--                                <a class="d-flex align-items-center" href="#">--}}
-{{--                                    <div class="notification-icon bg-success">--}}
-{{--                                        <i class="bi bi-file-earmark-check"></i>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="notification-text ms-4">--}}
-{{--                                        <p class="notification-title font-bold">Homework submitted</p>--}}
-{{--                                        <p class="notification-subtitle font-thin text-sm">Algebra math--}}
-{{--                                            homework</p>--}}
-{{--                                    </div>--}}
-{{--                                </a>--}}
-{{--                            </li>--}}
-{{--                            <li>--}}
-
-
-{{--                                <h6>Новые сообщение</h6>--}}
-
-{{--                            </li>--}}
-{{--                            @foreach($newMessage as $mess)--}}
-
-{{--                                <li class="dropdown-item notification-item">--}}
-{{--                                    <a class="d-flex align-items-center"--}}
-{{--                                       href="{{ route('client.removeNotification',$mess->task_id) }}">--}}
-{{--                                        <div class="notification-icon">--}}
-{{--                                            <i class="bi bi-bell text-primary"></i>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="notification-text ms-4">--}}
-{{--                                            <p class="notification-title font-bold">--}}
-{{--                                                <b>SMS:</b>{{ \Str::limit($mess->message, 10)  }}</p>--}}
-{{--                                            <p class="notification-subtitle font-thin text-sm d-flex">--}}
-{{--                                                <b>Задача:</b>{{ $mess->tasks->name }}</p>--}}
-{{--                                        </div>--}}
-{{--                                    </a>--}}
-{{--                                </li>--}}
-{{--                            @endforeach--}}
-
-{{--                        </ul>--}}
-{{--                    </li>--}}
-                </ul>
                 <div class="dropdown">
                     <a href="#" data-bs-toggle="dropdown" aria-expanded="false">
                         <div class="user-menu d-flex">
