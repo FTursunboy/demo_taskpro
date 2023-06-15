f9b6769b0c06d5df19fad542c78db09e7e92cc40
Tursunboy Faiziev <107097209+FTursunboy@users.noreply.github.com>
2023-06-15 13:46:27 +0500

    Update create.blade.php

--- resources/views/admin/tasks/create.blade.php
+++ resources/views/admin/tasks/create.blade.php
@@ -136,7 +136,7 @@
 
         FilePond.setOptions({
             server: {
-                url: 'tasks/public/upload',
+                url: 'tasks/public/upload/',
                 headers: {
                     'X-CSRF-TOKEN': '{{csrf_token()}}'
                 }
