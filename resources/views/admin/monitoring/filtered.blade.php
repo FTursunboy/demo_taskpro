fa6301ec4d0a65d9c2ba86a9e709207c620f6eac
Tursunboy Faiziev <107097209+FTursunboy@users.noreply.github.com>
2023-06-22 18:51:50 +0500

    Update filtered.blade.php

--- resources/views/admin/monitoring/filtered.blade.php
+++ resources/views/admin/monitoring/filtered.blade.php
@@ -9,7 +9,7 @@
         <div class="page-title">
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
-                    <h3>Мониторинг</h3>
+                    <h3>Мониторинг / Все задачи</h3>
                 </div>
                 <div class="col-12 col-md-6 order-md-2 order-first">
                     <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
