ce88f3f05502554cdd1de2740c6296d14ffc6399
Tursunboy Faiziev <107097209+FTursunboy@users.noreply.github.com>
2023-06-23 09:05:48 +0500

    123

--- resources/views/admin/incs/header.blade.php
+++ resources/views/admin/incs/header.blade.php
@@ -27,6 +27,11 @@
                    role="button">
                     <i class="bi bi-wallet" style="font-size: 30px;"></i>
                 </a>
+                <a data-bs-toggle="offcanvas" data-bs-target="#Statistic"
+                   aria-controls="Statistic" style="margin-left: 20px;"
+                   role="button">
+                    <i style="font-size: 31px" class="bi bi-speedometer"></i>
+                </a>
 
 {{--                @if(count($birthdayUsers) > 1)--}}
 {{--                    <div class="dropdown" style="margin-left: 300px;">--}}
@@ -159,6 +164,8 @@
                 </div>
 
             </div>
+
+
         </div>
     </nav>
 </header>
@@ -896,5 +903,134 @@
     </div>
 </div>
 {{--  Project ofcanvas  end  --}}
+
+<div class="offcanvas offcanvas-bottom" data-bs-backdrop="static" tabindex="-1" id="Statistic"
+     aria-labelledby="Statistic" style="width: 100%; height: 90%;">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="Statistic">Проекты</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header "></div>
+                <div class="card-body overflow-auto">
+                    <div class="row">
+                        <div class="col-9"></div>
+                        <div class="col">
+                            <div class="form-group">
+                                <select class="form-select" name="month" id="month">
+                                    <option value="0">фильтр по месяцу</option>
+                                    <option value="1">Январь</option>
+                                    <option value="2">Февраль</option>
+                                    <option value="3">Март</option>
+                                    <option value="4">Апрель</option>
+                                    <option value="5">Май</option>
+                                    <option value="6">Июнь</option>
+                                    <option value="7">Июль</option>
+                                    <option value="8">Август</option>
+                                    <option value="9">Сентябрь</option>
+                                    <option value="10">Октябрь</option>
+                                    <option value="11">Ноябрь</option>
+                                    <option value="12">Декабрь</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <table id="example" class="table table-border table-hover">
+                        <thead>
+                        <tr>
+                            <th class="text-center">#</th>
+                            <th class="text-center">ФИО</th>
+                            <th class="text-center">Все задачи</th>
+                            <th class="text-center">Долг</th>
+                            <th class="text-center">В процессе</th>
+                            <th class="text-center">Готово</th>
+                            <th class="text-center">Просроченное</th>
+                            <th class="text-center">Ожидается (Сотрудник)</th>
+                            <th class="text-center">На проверке (У админа)</th>
+                            <th class="text-center">На проверке (У клиента)</th>
+                            <th class="text-center">Отклонено (Администратором)</th>
+                            <th class="text-center">Отклонено (Сотрудником)</th>
+                        </tr>
+                        </thead>
+                        <tbody id="tableBodyMonitoring">
+                        @foreach($statistics as $user)
+                            <tr>
+                                <td class="text-center">{{$loop->iteration }}</td>
+                                <td>{{ \Illuminate\Support\Str::limit($user->name . " " . $user->surname, 50)  }}</td>
+                                <td class="text-center">{{ $user->usersCountTasks($user->id)['total'] }}</td>
+                                <td class="text-center">{{ $user->usersCountTasks($user->id)['debt'] }}</td>
+                                <td class="text-center">{{ $user->usersCountTasks($user->id)['process'] }}</td>
+                                <td class="text-center">{{ $user->usersCountTasks($user->id)['ready'] }}</td>
+                                <td class="text-center">{{ $user->usersCountTasks($user->id)['speed'] }}</td>
+                                <td class="text-center">{{ $user->usersCountTasks($user->id)['expectedUser'] }}</td>
+                                <td class="text-center">{{ $user->usersCountTasks($user->id)['forVerificationAdmin'] }}</td>
+                                <td class="text-center">{{ $user->usersCountTasks($user->id)['forVerificationClient'] }}</td>
+                                <td class="text-center">{{ $user->usersCountTasks($user->id)['rejectedAdmin'] }}</td>
+                                <td class="text-center">{{ $user->usersCountTasks($user->id)['rejectedClient'] }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        @routes
+        <script>
+            $(document).ready(function () {
+                var table = $('#example').DataTable({
+                    initComplete: function () {
+
+                    },
+                });
+
+                $('#month').on('change', function () {
+                    filterStatistic()
+                });
+
+                function filterStatistic() {
+                    let month = $('#month').val();
+
+                    $.get(`/tasks/public/monitoring-statistics-filter/${month}`, function (response) {
+                        let tableBody = $('#tableBodyMonitoring');
+                        table.clear().draw();
+                        tableBody.empty()
+
+                        if (response.statistics.length > 0) {
+                            buildTable(response.statistics, tableBody);
+                        }
+
+                    });
+                }
+
+                function buildTable(data, tableBody) {
+                    $.each(data, function (i, item) {
+                        let row = `
+                <tr>
+                    <td class="text-center">${i + 1}</td>
+                    <td>${item.user}</td>
+                    <td class="text-center">${item.total !== null ? item.total : 0}</td>
+                    <td class="text-center">${item.debt !== null ? item.debt : 0}</td>
+                    <td class="text-center">${item.process !== null ? item.process : 0}</td>
+                    <td class="text-center">${item.ready !== null ? item.ready : 0}</td>
+                    <td class="text-center">${item.speed !== null ? item.speed : 0}</td>
+                    <td class="text-center">${item.expectedUser !== null ? item.expectedUser : 0}</td>
+                    <td class="text-center">${item.forVerificationAdmin !== null ? item.forVerificationAdmin : 0}</td>
+                    <td class="text-center">${item.forVerificationClient !== null ? item.forVerificationClient : 0}</td>
+                    <td class="text-center">${item.rejectedAdmin !== null ? item.rejectedAdmin : 0}</td>
+                    <td class="text-center">${item.rejectedClient !== null ? item.rejectedClient : 0}</td>
+                </tr>`;
+
+                        tableBody.append(row);
+                    });
+                }
+
+
+            });
+        </script>
+
+    </div>
+</div>
 
 
