923440519515dd2ecc5118c68ec4d83a2634ee28
Tursunboy Faiziev <107097209+FTursunboy@users.noreply.github.com>
2023-06-24 10:06:34 +0500

    Update IndexController.php

--- app/Http/Controllers/User/IndexController.php
+++ app/Http/Controllers/User/IndexController.php
@@ -98,12 +98,14 @@ class IndexController extends BaseController
             ->whereMonth('to', Carbon::now()->month)
             ->get()
             ->count();
-        $tasks_ready = TaskModel::where([
-            ['user_id', Auth::id()],
-            ['status_id', [3, 10]]
-        ])->whereMonth('from', Carbon::now()->month)->whereMonth('to', Carbon::now()->month)
+   
+        $tasks_ready = TaskModel::where('user_id', Auth::id())
+            ->whereIn('status_id', [3, 10])
+            ->whereMonth('from', Carbon::now()->month)
+            ->whereMonth('to', Carbon::now()->month)
             ->get()
             ->count();
+
         $result = ($tasks_ready / $tasks) * 100;
 
         return response([
