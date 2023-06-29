51de2c879198f7d088ca33f6a2d108433a1233a5
Khusrav-Muhammadi <117286325+Khusrav-Muhammadi@users.noreply.github.com>
2023-06-29 08:56:31 +0500

    commit

--- resources/views/user/incs/header.blade.php
+++ resources/views/user/incs/header.blade.php
@@ -64,11 +64,10 @@
                         style="min-width: 11rem;">
                         <li><a class="dropdown-item" href="{{ route('user_profile.index', auth()->id())}}"><i
                                         class="icon-mid bi bi-person me-2"></i>Мой профиль</a></li>
-
                         <hr class="dropdown-divider">
                         <li><a role="button" class='dropdown-item' data-bs-toggle="modal"
                                data-bs-target="#staticBackdrop"><i
-                                        class="icon-mid bi bi-box-arrow-left me-2"></i> Выход</a></li>
+                                        class="icon-mid bi bi-box-arrow-left me-2"></i>Выход</a></li>
                     </ul>
                 </div>
             </div>
