08e6870ed4dbaaf9abe25c81df89ea20aaddbdc8
Khusrav-Muhammadi <117286325+Khusrav-Muhammadi@users.noreply.github.com>
2023-07-07 09:07:00 +0500

    fvdc

--- resources/views/user/all-tasks/index.blade.php
+++ resources/views/user/all-tasks/index.blade.php
@@ -65,7 +65,11 @@
                 function buildTable(data, table) {
                     $.each(data, function (i, item) {
                         var routeName = 'all-tasks.show'; // Замените 'route.name' на фактическое имя маршрута в Laravel
+<<<<<<< Updated upstream
                         var routeUrl = "{{ route('/all-tasks.show', ':slug') }}"; // Замените 'route.name' на фактическое имя маршрута в Laravel
+=======
+                        var routeUrl = "{{ route('all-tasks.show', ':slug') }}"; // Замените 'route.name' на фактическое имя маршрута в Laravel
+>>>>>>> Stashed changes
                         routeUrl = routeUrl.replace(':slug', item.slug); // Замените 'slug' на фактический параметр маршрута
 
                         table.row.add([
