00cbd35cf4640397bfd8f94084d7d9dc6eeef3a4
Radzkhabov Rustam <rustam.radzabov8410@gmail.com>
2023-07-07 16:18:23 +0500

    вфы

--- app/Http/Controllers/API/V1/Statistics/StatisticController.php
+++ app/Http/Controllers/API/V1/Statistics/StatisticController.php
@@ -13,20 +13,24 @@ class StatisticController extends Controller
     {
         $projectTasksAdmin = ProjectModel::withCount('tasks')->orderByDesc('tasks_count')->get();
 
-        $response = $projectTasksAdmin->map(function ($project) {
-            return [
-                'name' => $project->name,
-                'count_tasks' => $project->tasks_count,
-                'count_ready' => $project->count_ready(),
-                'count_process' => $project->count_process(),
-                'count_verificateClient' => $project->count_verificateClient(),
-                'count_verificateAdmin' => $project->count_verificateAdmin(),
-                'count_outOfDate' => $project->count_outOfDate(),
-                'count_other' => $project->count_other(),
-            ];
-        });
+        $response = [
+            'ProjectTasks' => $projectTasksAdmin->map(function ($project) {
+                return [
+                    'name' => $project->name,
+                    'count_tasks' => $project->tasks_count,
+                    'count_ready' => $project->count_ready(),
+                    'count_process' => $project->count_process(),
+                    'count_verificateClient' => $project->count_verificateClient(),
+                    'count_verificateAdmin' => $project->count_verificateAdmin(),
+                    'count_outOfDate' => $project->count_outOfDate(),
+                    'count_other' => $project->count_other(),
+                ];
+            }),
+            'message' => true,
+        ];
 
         return response($response, 200);
     }
+
 
 }
