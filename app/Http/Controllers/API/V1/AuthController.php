4b167d0216b79efd7a8d27852c3570c5fdeeb7a1
Khusrav-Muhammadi <117286325+Khusrav-Muhammadi@users.noreply.github.com>
2023-07-18 13:48:48 +0500

    commit

--- app/Http/Controllers/API/V1/AuthController.php
+++ app/Http/Controllers/API/V1/AuthController.php
@@ -7,12 +7,23 @@ use App\Http\Resources\API\V1\Auth\AuthResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
         $user = User::where('login', $request->login)->first();
+
+        if ($request->file('avatar') !== null) {
+            if ($user->avatar !== null) {
+                Storage::disk('public')->delete($user->avatar);
+            }
+            $file = Storage::disk('public')->put('/user_img', $request->file('avatar'));
+        } else {
+            $file = $user->avatar;
+        }
+
         if (!$user || !Hash::check($request->password, $user->password)) {
             return response()->json([
                 'message' => 'Пароль или логин введены неправильно'
@@ -24,6 +35,7 @@ class AuthController extends Controller
             'message' => true,
             'token' => $token,
             'user' => new AuthResource($user),
+            'avatar' => '/tasks/public'.Storage::url($file)
         ]);
     }
 
