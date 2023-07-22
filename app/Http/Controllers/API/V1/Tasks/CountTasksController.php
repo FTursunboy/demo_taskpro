047c83c63bc7ef0fd8ed66a0efde8b669851e9ed
Khusrav-Muhammadi <117286325+Khusrav-Muhammadi@users.noreply.github.com>
2023-07-22 09:38:47 +0500

    commit

--- app/Http/Controllers/API/V1/Tasks/CountTasksController.php
+++ app/Http/Controllers/API/V1/Tasks/CountTasksController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\API\V1\Tasks;
 
 use App\Http\Controllers\Controller;
 use App\Models\Admin\TaskModel;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CountTasksController extends Controller
 {
@@ -18,6 +20,41 @@ class CountTasksController extends Controller
            'verificate_client' => TaskModel::where('status_id', 10)->count(),
            'reject_client' => TaskModel::where('status_id', 13)->count(),
            'archive' => TaskModel::where('status_id', 3)->count()
+        ]);
+    }
+
+    public function panelUser()
+    {
+        $endDate = Carbon::now()->addDays(7)->format('Y-m-d');
+
+        return response([
+           'ready' => TaskModel::where([
+               ['user_id', '=', Auth::id()],
+               ['status_id', '=', 3]
+           ])->count(),
+           '7_days_left' => TaskModel::where([
+               ['user_id', '=', Auth::id()],
+               ['to', '<=', $endDate],
+           ])
+            ->whereIn('status_id', [2, 4])->count(),
+           'speed' => TaskModel::where([
+               ['user_id', '=', Auth::id()],
+               ['status_id', '=', 7],
+           ])->count(),
+           'verificate_admin' => TaskModel::where([
+               ['user_id', '=', Auth::id()],
+               ['status_id', '=', 6]
+           ])->count(),
+            'in_progress' => TaskModel::where([
+                ['user_id', '=', Auth::id()],
+            ])->whereIn('status_id', [2, 4])->count(),
+            'new' => TaskModel::where([
+                ['user_id', '=', Auth::id()]
+            ])->whereIn('status_id', [1, 9])->count(),
+            'all' => TaskModel::where([
+               ['user_id', '=', Auth::id()],
+               ['status_id', '!=', 3]
+           ])->count()
         ]);
     }
 }
