c7c6ca8297e8b217386ac1f3f49eb4bfe77354a6
Khusrav-Muhammadi <117286325+Khusrav-Muhammadi@users.noreply.github.com>
2023-07-25 10:09:10 +0500

    л

--- app/Models/User.php
+++ app/Models/User.php
@@ -412,38 +412,19 @@ class User extends Authenticatable
     }
 
 
-//    public function debt($month, $id)
-//    {
-//        $currentYear = Carbon::now()->year;
-//        $startOfYear = Carbon::now()->year($currentYear)->startOfYear();
-//        $endOfMonth = Carbon::now()->year($currentYear)->month($month - 1)->endOfMonth();
-//
-//        $debt = TaskModel::where([
-//            ['user_id', $id],
-//            ['status_id', '!=', 3]
-//        ])->whereBetween('to', [$startOfYear, $endOfMonth])->get()->count();
-//
-//        return $debt;
-//    }
-
     public function debt($month, $id)
     {
-        $selectedMonth = Carbon::createFromDate(null, $month, 1);
-        $startOfMonth = $selectedMonth->startOfMonth();
+        $currentYear = Carbon::now()->year;
+        $startOfYear = Carbon::now()->year($currentYear)->startOfYear();
+        $endOfMonth = Carbon::now()->year($currentYear)->month($month - 1)->endOfMonth();
 
         $debt = TaskModel::where([
             ['user_id', $id],
-            ['status_id', '!=', 3],
-            ['to', '<', $startOfMonth]
-        ])->count();
+            ['status_id', '!=', 3]
+        ])->whereBetween('to', [$startOfYear, $endOfMonth])->get()->count();
 
         return $debt;
     }
-
-
-
-
-
 
 
 
