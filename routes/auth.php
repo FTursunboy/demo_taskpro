8c5b7fee73c8a7aa03b67a82b7a979dd6b1febe5
Tursunboy Faiziev <107097209+FTursunboy@users.noreply.github.com>
2023-08-01 20:35:35 +0500

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+    Route::get('lodddgin', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
 
     Route::post('logifsdn', [AuthenticatedSessionController::class, 'store1']);
