058c7c42eaa09ca27d16b54ea0317d64bea9d736
Tursunboy Faiziev <107097209+FTursunboy@users.noreply.github.com>
2023-08-01 19:52:16 +0500

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+    Route::get('login', [\App\Http\Controllers\Admin\IndexController::class, 'index'])
                 ->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
