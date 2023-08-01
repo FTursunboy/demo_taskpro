28fb33cf126b3edb3d9f9b4783ddc5e247b85245
Tursunboy Faiziev <107097209+FTursunboy@users.noreply.github.com>
2023-08-01 19:57:02 +0500

    234

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use \Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     return view('auth.login');
-})->name('login');
+})->name('gdf');
 
 
 Route::group(['middleware' => 'redirectIfUnauthorized'], function () {
