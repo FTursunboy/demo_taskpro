c875289c8477f3232e948a594c7e3d2c2f52d959
nuriddinshahobov2001 <nuriddinshahobov2001>
2023-08-02 11:18:19 +0300

    gsd

---
+++ storage/framework/views/dfa87e3230dba216845487a3bbbcb3e2.php
@@ -0,0 +1,9 @@
+<script src="<?php echo e(asset('assets1/libs/jquery/jquery.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets1/libs/bootstrap/js/bootstrap.bundle.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets1/libs/metismenu/metisMenu.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets1/libs/simplebar/simplebar.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets1/libs/node-waves/waves.min.js')); ?>"></script>
+<script src="<?php echo e(asset('assets1/libs/feather-icons/feather.min.js')); ?>"></script>
+<!-- pace js -->
+<script src="<?php echo e(asset('assets1/libs/pace-js/pace.min.js')); ?>"></script>
+<?php /**PATH /home/c/cx34222/task_manager/public_html/tasks/resources/views/user/vendor_scripts.blade.php ENDPATH**/ ?>
